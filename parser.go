140f1fca29a7d676a934e046584e436e93138c72
HankPeeples <hgpeeples48@gmail.com>
2022-05-31 13:31:49 -0500

    Added linkNodes function to find and return all links

--- parser.go
+++ parser.go
@@ -1,6 +1,11 @@
 package linkParser
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link (<a href="">...</a>) in an HTML document.
 type Link struct {
@@ -10,5 +15,44 @@ type Link struct {
 
 // Parse will take in an HTML document and return a slice of Links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := linkNodes(doc)
+
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
+
+	// dfs(doc, "")
+	// 1. Find <a> nodes in the document
+	// 2. For each link node...build a link
+	// 3. Return the links
 	return nil, nil
 }
+
+func linkNodes(n *html.Node) []*html.Node {
+	// base case
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
+}
+
+// func dfs(n *html.Node, padding string) {
+// 	msg := n.Data
+// 	if n.Type == html.ElementNode {
+// 		msg = "<" + msg + ">"
+// 	}
+// 	fmt.Println(padding, msg)
+// 	for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 		dfs(c, padding+"  ")
+// 	}
+// }
