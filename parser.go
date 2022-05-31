b982fcca5d90ad1465c8678cc4166b69e2222521
HankPeeples <hgpeeples48@gmail.com>
2022-05-31 13:07:49 -0500

    Added Link struct and Parse() function

--- parser.go
+++ parser.go
@@ -2,6 +2,13 @@ package linkParser
 
 import "io"
 
-func Parse(r io.Reader) (io.Reader, error) {
+// Link represents a link (<a href="">...</a>) in an HTML document.
+type Link struct {
+	Href string
+	Text string
+}
+
+// Parse will take in an HTML document and return a slice of Links parsed from it.
+func Parse(r io.Reader) ([]Link, error) {
 	return nil, nil
 }
